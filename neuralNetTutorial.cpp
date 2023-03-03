50515b5318913d96284ddf4fa0659faa21945f5f
andydo1122 <doandy12345@gmail.com>
2023-03-03 14:55:18 -0800

    update neuralNetTutorial.cpp
    Add it's data types

--- neuralNetTutorial.cpp
+++ neuralNetTutorial.cpp
@@ -1,6 +1,14 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 class Net{
     public:
-
+        Net(const vector<unsigned> &topology);
+        void feedForward(const vector<double> &inputVals);
+        void backProp(const vector<double> &targetVals);
+        void getResults(vector<double> &resultVals) const;
 
     private:
 
@@ -10,13 +18,18 @@ class Net{
 
 int main(){
 
+    // e.g. (3, 2, 1)
+    vector<unsigned> topology;
     Net myNet(topology);
-
+    
+    vector<double> inputVals;
     myNet.feedForward(inputVals);
 
     // During training
+    vector<double> targetVals;
     myNet.backProp(targetVals);
 
     // Neural network's outputs
+    vector<double> resultVals;
     myNet.getResults(resultVals);
 }
