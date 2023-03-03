bf6d99e0ae060f2e89c337eef3e14850ce789fe3
andydo1122 <doandy12345@gmail.com>
2023-03-03 15:05:47 -0800

    update neuralNetworkTutorial.cpp
    Test and compile

--- neuralNetTutorial.cpp
+++ neuralNetTutorial.cpp
@@ -3,18 +3,34 @@
 
 using namespace std;
 
+class Neuron;
+typedef vector<Neuron> Layer;
+
 class Net{
     public:
         Net(const vector<unsigned> &topology);
-        void feedForward(const vector<double> &inputVals);
-        void backProp(const vector<double> &targetVals);
-        void getResults(vector<double> &resultVals) const;
+        void feedForward(const vector<double> &inputVals) {};
+        void backProp(const vector<double> &targetVals) {};
+        void getResults(vector<double> &resultVals) const {};
 
     private:
-
+        vector<Layer> n_layers; // n_layers[layerNum][neuronNum]
 
 };
 
+Net::Net(const vector<unsigned> &topology){
+    unsigned numLayers = topology.size();
+    for(unsigned layerNum = 0; layerNum < numLayers; ++layerNum){
+        n_layers.push_back(Layer());
+
+        // Created a new Layer, now we need to fill it
+        // with neurons, and add a bias neuron to the layer
+
+        for(unsigned neuronNum = 0; neuronNum <= topology[layerNum]; ++neuronNum){
+            n_layers.back().push_back(Neuron()); 
+        }
+    }
+}
 
 int main(){
 
